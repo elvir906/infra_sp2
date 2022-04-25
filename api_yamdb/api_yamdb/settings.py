3c0e79aaa904324666f31ddf53b00297d0485bae
elvir906 <himynameiselvir@gmail.com>
2022-04-25 23:53:29 +0500

    исправил базу2

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,8 +1,6 @@
 from datetime import timedelta
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
